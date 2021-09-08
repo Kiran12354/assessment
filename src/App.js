ebb18780e8161ca797f8bbba7e94d195e0e0ca34
Kiran12354 <kirankumar9380@gmail.com>
2021-09-08 13:04:55 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Giphyapi from "./Giphyapi";
+import Upload from "./Upload";
+
+function App() {
+  return (
+    <>
+     <Upload/>
+     <Giphyapi/>
+    </>
+  );
+}
+
+export default App;
